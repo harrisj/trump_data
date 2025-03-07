6b3d55f3215d970c80b8535faa667e70497d7ee5
bcks <john@backspace.com>
2025-03-07 10:21:59 -0500

    Only add new golf dates from Factbase feed

--- home_visits/scrape_golf.py
+++ home_visits/scrape_golf.py
@@ -8,25 +8,27 @@ def extract_golf_dates(data, output_file):
 
     golf_dates = []
 
-    # Don't include that one diary entry for Trump National Doral Miami when after 9:30pm
-    def is_too_late_for_golf(time_string):
-        if time_string == None:
-            return
-        time_format = "%H:%M:%S"
-        time_obj = datetime.strptime(time_string, time_format)
-        nine_pm = datetime.strptime("21:30:00", time_format)
-        return time_obj < nine_pm
+    # read previous file and load dates into golf_dates list
+    try:
+        with open(output_file, "r") as f:
+            reader = csv.DictReader(f)
+            golf_dates = [
+                {"date": row["date"], "location": row["location"]} for row in reader
+            ]
+    except FileNotFoundError:
+        # If the file doesn't exist yet, start with an empty list
+        pass
 
-    # Process each entry
+    previous_length = len(golf_dates)
+
+    # Process each entry from json feed
     for i, entry in enumerate(data):
 
-        if entry.get("tags") == {"golf": True} or (
-            (entry.get("location") == "Trump National Doral Miami")
-            and is_too_late_for_golf(entry.get("time"))
-        ):
+        if entry.get("tags") == {"golf": True}:
             date = entry.get("date")
             location = entry.get("location")
 
+            # does it already exist?
             if {"date": date, "location": location} in golf_dates:
                 continue
 
@@ -39,11 +41,11 @@ def extract_golf_dates(data, output_file):
 
     # Write to CSV
     with open(output_file, "w") as f:
-        writer = csv.DictWriter(f, fieldnames=["date", "location"], lineterminator='\n')
+        writer = csv.DictWriter(f, fieldnames=["date", "location"], lineterminator="\n")
         writer.writeheader()
         writer.writerows(sorted_by_date)
 
-    print("Processed {} Golf entries.".format(len(golf_dates)))
+    print("Added {} new golf entries.".format(len(golf_dates) - previous_length))
 
 
 def main():
@@ -55,7 +57,6 @@ def main():
         extract_golf_dates(data, output_file)
     else:
         print("Failed to retrieve data: {}".format(response.status_code))
-
 
 
 if __name__ == "__main__":
