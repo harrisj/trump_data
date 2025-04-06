34888056cf0552ba082acaeecfd9426fa0031884
Jacob Harris <harrisj.home@gmail.com>
2025-04-06 13:32:33 -0400

    Export to events.yaml from modernization file

--- .hooks/modernization_events.sh
+++ .hooks/modernization_events.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd ./it_modernization
-pipenv run python dump_events.py
-pipenv run python dump_people.py
+pipenv run python scripts/dump_events.py
+pipenv run python scripts/dump_people.py
 
