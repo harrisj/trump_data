1d2d538bcfd9f9097f4234c44efae80f75b5526e
Jacob Harris <harrisj.home@gmail.com>
2025-04-07 12:12:07 -0400

    Trying another test of the pre-commit hook

--- it_modernization/scripts/sort_events_yaml.py
+++ it_modernization/scripts/sort_events_yaml.py
@@ -33,7 +33,7 @@ file_path = './data/events.yaml'
 people_data = read_events_yaml(file_path)
 output_file = './data/events_sorted.yaml'
 with open(output_file, 'w') as file:
-    file.write("# yaml-language-server: $schema=schemas/events-file.json\n")
+    file.write("# yaml-language-server: $schema=../schemas/events-file.json\n")
     file.write(yaml.dump(people_data, Dumper=YamlDumper, indent=2, width=500, sort_keys=False))
 
 os.replace(output_file, file_path)
