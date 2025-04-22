e3d102542429f234658539d3fb16b70d29aa5f73
Jacob Harris <harrisj.home@gmail.com>
2025-04-22 13:52:23 -0400

    A few corrections

--- it_modernization/scripts/preprocess/01_preprocess_events_yaml.py
+++ it_modernization/scripts/preprocess/01_preprocess_events_yaml.py
@@ -22,9 +22,9 @@ def read_events(file_path):
 
 
 # Example usage
-file_path = "./raw_data/events.yaml"
+src_path = "raw_data/events.yaml"
 events_data = read_events(file_path)
-output_file = "./raw_data/events_sorted.yaml"
+output_file = "raw_data/events_sorted.yaml"
 with open(output_file, "w") as file:
     file.write("# yaml-language-server: $schema=../schemas/events-file.json\n")
     file.write(
