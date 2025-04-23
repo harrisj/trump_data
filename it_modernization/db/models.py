84b93bd96b3d42da8f5cfd8da58642287462487f
Jacob Harris <harrisj.home@gmail.com>
2025-04-23 16:40:21 -0400

    Peewee code is working!!

---
+++ it_modernization/db/models.py
@@ -0,0 +1,196 @@
+from peewee import *
+from edtf import parse_edtf
+
+
+class EdtfField(Field):
+    field_type = "uuid"
+
+    def db_value(self, value):
+        return str(value)
+
+    def python_value(self, value):
+        return parse_edtf(value)
+
+
+db = SqliteDatabase("doge.sqlite")
+
+
+# model definitions -- the standard "pattern" is to define a base model class
+# that specifies which database to use.  then, any subclasses will automatically
+# use the correct storage.
+class BaseModel(Model):
+    class Meta:
+        database = db
+        legacy_table_names = False
+
+
+class Agency(BaseModel):
+    id = TextField(null=True, primary_key=True)
+    name = TextField(null=True)
+    parent = ForeignKeyField("self", null=True, backref="children")
+
+
+class Alias(BaseModel):
+    id = TextField(null=True, primary_key=True)
+    agency = ForeignKeyField(
+        Agency, column_name="agency_id", field="id", backref="aliases"
+    )
+    evidence = TextField(null=True)
+    name = TextField(null=True)
+
+
+DetailPersonDeferred = DeferredThroughModel()
+EventPersonDeferred = DeferredThroughModel()
+EventAliasDeferred = DeferredThroughModel()
+EventAgencyDeferred = DeferredThroughModel()
+EventSystemDeferred = DeferredThroughModel()
+DetailAliasDeferred = DeferredThroughModel()
+CaseAgencyDeferred = DeferredThroughModel()
+
+
+class Person(BaseModel):
+    name = TextField(null=True, primary_key=True)
+    sort_name = TextField(null=True)
+    age = TextField(null=True)
+    background = TextField(null=True)
+
+
+class Detail(BaseModel):
+    id = TextField(null=True, primary_key=True)
+    from_ = TextField(column_name="from", null=True)
+    to = TextField(null=True)
+    comment = TextField(null=True)
+    file = TextField(null=True)
+    max_people = TextField(null=True)
+    named = TextField(null=True)
+    named_aliases = TextField(null=True)
+    nte_date = TextField(null=True)
+    reimbursed = TextField(null=True)
+    reimbursement_amount = TextField(null=True)
+    signed_date = TextField(null=True)
+    source = TextField(null=True)
+    start_date = TextField(null=True)
+    people = ManyToManyField(
+        Person, backref="details", through_model=DetailPersonDeferred
+    )
+    aliases = ManyToManyField(
+        Alias, backref="details", through_model=DetailAliasDeferred
+    )
+
+
+class CourtCase(BaseModel):
+    case_no = TextField(primary_key=True)
+    date_filed = TextField(null=True)
+    description = TextField(null=True)
+    link = TextField(null=True)
+    name = TextField(null=True)
+    status = TextField(null=True)
+    agencies = ManyToManyField(
+        Agency, backref="agencies", through_model=CaseAgencyDeferred
+    )
+
+
+class Event(BaseModel):
+    id = TextField(null=True, primary_key=True)
+    type = TextField(null=True)
+    date = EdtfField()
+    sort_date = TextField()
+    event = TextField()
+    source = TextField()
+
+    access_type = TextField(null=True)
+    case = ForeignKeyField(
+        CourtCase, column_name="case_no", field="case_no", null=True, backref="events"
+    )
+    comment = TextField(null=True)
+
+    detail = ForeignKeyField(
+        Detail, column_name="detail_id", field="id", null=True, backref="events"
+    )
+    detailed_from = TextField(null=True)
+
+    fuzz = TextField(null=True)
+    onboard_type = TextField(null=True)
+
+    people = ManyToManyField(
+        Person, backref="events", through_model=EventPersonDeferred
+    )
+    agencies = ManyToManyField(
+        Agency, backref="events", through_model=EventAgencyDeferred
+    )
+    aliases = ManyToManyField(Alias, backref="events", through_model=EventAliasDeferred)
+
+
+class System(BaseModel):
+    id = TextField(null=True, primary_key=True)
+    type = TextField(null=True)
+
+    category = TextField(null=True)
+    comment = TextField(null=True)
+    description = TextField(null=True)
+    link = TextField(null=True)
+    name = TextField(null=True)
+    pia = TextField(null=True)
+    population = TextField(null=True)
+    risk = TextField(null=True)
+    sorns = TextField(null=True)
+
+    events = ManyToManyField(
+        Event, backref="access_systems", through_model=EventSystemDeferred
+    )
+
+
+class CourtCaseAgencyThrough(BaseModel):
+    agency = ForeignKeyField(Agency, column_name="agency_id", field="id")
+    court_case = ForeignKeyField(CourtCase, column_name="case_no", field="case_no")
+
+
+CaseAgencyDeferred.set_model(CourtCaseAgencyThrough)
+
+
+class DetailAliasThrough(BaseModel):
+    detail = ForeignKeyField(Detail, column_name="detail_id", field="id")
+    alias = ForeignKeyField(Alias, column_name="alias", field="id")
+
+
+DetailAliasDeferred.set_model(DetailAliasThrough)
+
+
+class DetailPersonThrough(BaseModel):
+    detail = ForeignKeyField(Detail, column_name="detail_id", field="id")
+    person = ForeignKeyField(Person, column_name="name", field="name")
+
+
+DetailPersonDeferred.set_model(DetailPersonThrough)
+
+
+class EventAgencyThrough(BaseModel):
+    event = ForeignKeyField(Event, column_name="event_id", field="id")
+    agency = ForeignKeyField(Agency, column_name="agency_id", field="id")
+
+
+EventAgencyDeferred.set_model(EventAgencyThrough)
+
+
+class EventAliasThrough(BaseModel):
+    event = ForeignKeyField(Event, column_name="event_id", field="id")
+    alias = ForeignKeyField(Alias, column_name="alias", field="id")
+
+
+EventAliasDeferred.set_model(EventAliasThrough)
+
+
+class EventPersonThrough(BaseModel):
+    event = ForeignKeyField(Event, column_name="event_id", field="id")
+    name = ForeignKeyField(Person, column_name="name", field="name")
+
+
+EventPersonDeferred.set_model(EventPersonThrough)
+
+
+class EventSystemThrough(BaseModel):
+    event = ForeignKeyField(Event, column_name="event_id", field="id")
+    system = ForeignKeyField(System, column_name="system_id", field="id")
+
+
+EventSystemDeferred.set_model(EventSystemThrough)
--- it_modernization/scripts/03_load_sqlite.sh
+++ it_modernization/scripts/03_load_sqlite.sh
@@ -3,41 +3,41 @@ DB=./it_modernization/doge.sqlite
 PREFIX=./it_modernization/db/import
 rm -f $DB
 
-sqlite-utils insert $DB cases $PREFIX/cases.csv --csv --pk case_no
-sqlite-utils insert $DB events $PREFIX/events.csv --csv --pk id
-sqlite-utils insert $DB agencies $PREFIX/agencies.csv --csv --pk id
-sqlite-utils insert $DB aliases $PREFIX/aliases.csv --csv --pk id
-sqlite-utils insert $DB details $PREFIX/details.csv --csv --pk id
-sqlite-utils insert $DB systems $PREFIX/systems.csv --csv --pk id
-sqlite-utils insert $DB people $PREFIX/people.csv --csv --pk name
+sqlite-utils insert $DB court_case $PREFIX/cases.csv --csv --pk case_no --not-null name --not-null case_no --not-null link --not-null date_filed --silent
+sqlite-utils insert $DB event $PREFIX/events.csv --csv --pk id --not-null id --not-null date --not-null event --not-null type --silent
+sqlite-utils insert $DB agency $PREFIX/agencies.csv --csv --pk id --not-null id --not-null name --silent
+sqlite-utils insert $DB alias $PREFIX/aliases.csv --csv --pk id --not-null id --silent
+sqlite-utils insert $DB detail $PREFIX/details.csv --csv --pk id --not-null id --not-null start_date --not-null from --not-null to --not-null source --silent
+sqlite-utils insert $DB system $PREFIX/systems.csv --csv --pk id --not-null id --not-null name --not-null description --silent
+sqlite-utils insert $DB person $PREFIX/people.csv --csv --pk name --not-null name --not-null sort_name --silent
 
-sqlite-utils add-foreign-key $DB events detail_id details id
-sqlite-utils add-foreign-key $DB events case_no cases case_no
+sqlite-utils add-foreign-key $DB event detail_id detail id 
+sqlite-utils add-foreign-key $DB event case_no court_case case_no
 
-sqlite-utils insert $DB cases_agencies $PREFIX/cases_agencies.csv --csv
-sqlite-utils add-foreign-key $DB cases_agencies case_no cases case_no
-sqlite-utils add-foreign-key $DB cases_agencies agency_id agencies id
+sqlite-utils insert $DB court_case_agency_through $PREFIX/cases_agencies.csv --csv --silent
+sqlite-utils add-foreign-key $DB court_case_agency_through case_no court_case case_no
+sqlite-utils add-foreign-key $DB court_case_agency_through agency_id agency id
 
-sqlite-utils insert $DB details_aliases $PREFIX/details_aliases.csv --csv
-sqlite-utils add-foreign-key $DB details_aliases detail_id details id
-sqlite-utils add-foreign-key $DB details_aliases alias aliases id
+sqlite-utils insert $DB detail_alias_through $PREFIX/details_aliases.csv --csv --silent
+sqlite-utils add-foreign-key $DB detail_alias_through detail_id detail id
+sqlite-utils add-foreign-key $DB detail_alias_through alias alias id
 
-sqlite-utils insert $DB details_names $PREFIX/details_names.csv --csv
-sqlite-utils add-foreign-key $DB details_names detail_id details id
-sqlite-utils add-foreign-key $DB details_names name people name
+sqlite-utils insert $DB detail_person_through $PREFIX/details_names.csv --csv --silent
+sqlite-utils add-foreign-key $DB detail_person_through detail_id detail id
+sqlite-utils add-foreign-key $DB detail_person_through name person name
 
-sqlite-utils insert $DB events_agencies $PREFIX/events_agencies.csv --csv
-sqlite-utils add-foreign-key $DB events_agencies event_id events id
-sqlite-utils add-foreign-key $DB events_agencies agency_id agencies id
+sqlite-utils insert $DB event_agency_through $PREFIX/events_agencies.csv --csv --silent
+sqlite-utils add-foreign-key $DB event_agency_through event_id event id
+sqlite-utils add-foreign-key $DB event_agency_through agency_id agency id
 
-sqlite-utils insert $DB events_aliases $PREFIX/events_aliases.csv --csv
-sqlite-utils add-foreign-key $DB events_aliases event_id events id
-sqlite-utils add-foreign-key $DB events_aliases alias aliases id
+sqlite-utils insert $DB event_alias_through $PREFIX/events_aliases.csv --csv --silent
+sqlite-utils add-foreign-key $DB event_alias_through event_id event id
+sqlite-utils add-foreign-key $DB event_alias_through alias alias id
 
-sqlite-utils insert $DB events_names $PREFIX/events_names.csv --csv
-sqlite-utils add-foreign-key $DB events_names event_id events id
-sqlite-utils add-foreign-key $DB events_names names people name
+sqlite-utils insert $DB event_person_through $PREFIX/events_names.csv --csv --silent
+sqlite-utils add-foreign-key $DB event_person_through event_id event id
+sqlite-utils add-foreign-key $DB event_person_through name person name
 
-sqlite-utils insert $DB events_systems $PREFIX/events_systems.csv --csv
-sqlite-utils add-foreign-key $DB events_systems event_id events id
-sqlite-utils add-foreign-key $DB events_systems system_id systems id
+sqlite-utils insert $DB event_system_through $PREFIX/events_systems.csv --csv --silent
+sqlite-utils add-foreign-key $DB event_system_through event_id event id
+sqlite-utils add-foreign-key $DB event_system_through system_id system id
